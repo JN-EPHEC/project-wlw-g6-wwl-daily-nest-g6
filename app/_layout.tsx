71de5dd0478208e72378b0e5ef94fdce3f45c5d3
FZ-R <framhani@gmail.com>
2025-11-12 09:52:15 +0100

    12

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,6 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import React from "react";
-import Avantages from "./drawer/Avantages";
-import Budget from "./drawer/Budget";
-import Carnetfamiliale from "./drawer/Carnetfamiliale";
-import Deconnexion from "./drawer/Deconnexion";
-import Invitation from "./drawer/Invitation";
-import ListeCourse from "./drawer/ListeCourse";
-import Parametres from "./drawer/Parametres";
-import profil from "./drawer/profil";
-import Acceuil from "./tabs/_layout";
+import 'react-native-reanimated';
 
-export type DrawerMenuParamList = {
-  Accueil: undefined;
-  Carnetfamiliale: undefined;
-  ListeCourse: undefined;
-  Budget: undefined;
-  Invitation: undefined;
-  profil: undefined;
-  Parametres: undefined;
-  Avantages: undefined;
-  Deconnexion: undefined;
+export default function Rootlayout(){
+    return <slot />;
+
 };
-
-const Drawer = createDrawerNavigator<DrawerMenuParamList>();
-
-export default function DrawerNav() {
-  return (
-  <Drawer.Navigator>
-      <Drawer.Screen name="Accueil" component={Acceuil} options={{title: "Accueil"}}/>
-
-      <Drawer.Screen name="Carnetfamiliale" component={Carnetfamiliale}
-        options={{title: "Carnet Familial"}}/>
-
-      <Drawer.Screen name="ListeCourse" component={ListeCourse}
-        options={{title: "Liste de Courses"}}/>
-
-      <Drawer.Screen name="Budget" component={Budget} 
-      options={{title: "Budget",}}/>
-
-      <Drawer.Screen name="Invitation" component={Invitation}
-        options={{title: "Invitations"}}/>
-    
-      <Drawer.Screen name="profil" component={profil}
-        options={{title:"Profil" }}/>  
-
-       <Drawer.Screen name="Parametres" component={Parametres}
-        options={{title:"Parametres" }}/>  
-
-      <Drawer.Screen name="Avantages" component={Avantages}
-        options={{title:"Avantages" }}/>  
-      
-      <Drawer.Screen name="Deconnexion" component={Deconnexion}
-        options={{title:"Déconnexion" }}/>
-    </Drawer.Navigator>
-  );
-}
-
-
