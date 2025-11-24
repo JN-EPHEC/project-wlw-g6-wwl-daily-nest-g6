6699c39f6d5f05a0c9b9d6f534e81630862d53cd
amaurystra123-jpg <amaurystra123@gmail.com>
2025-11-24 14:52:32 +0100

    Revert "Aminé"

--- app/drawer/Acceuil.tsx
+++ app/drawer/Acceuil.tsx
@@ -1,17 +1,16 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Picker } from "@react-native-picker/picker";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { DrawerActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useState } from "react";
 import { Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { auth, db } from "../../firebaseConfig";
+import { db } from "../../firebaseConfig";
 
-import chat from "../tabs/chat";
 import Home from "../tabs/Home";
 import Recompense from "../tabs/Recompense";
 import ToDo from "../tabs/ToDo";
+import chat from "../tabs/chat";
 import Carnetfamiliale from "./Carnetfamiliale";
 import ListeCourse from "./ListeCourse";
 
@@ -39,33 +38,11 @@ export function Acceuil() {
   const [todoPerson, setTodoPerson] = useState("");
   const [todoDate, setTodoDate] = useState("");
 
-  const [shoppingLists, setShoppingLists] = useState<any[]>([]);
-const [selectedListId, setSelectedListId] = useState<string>("");
-const [newListName, setNewListName] = useState("");
-const [shoppingItem, setShoppingItem] = useState("");
-
-
+  const [shoppingList, setShoppingList] = useState("");
+  const [shoppingItem, setShoppingItem] = useState("");
 
   const goBack = () => { setModalScreen (null)};
   const [modalVisible, setModalVisible] = useState(false);
-  const user = auth.currentUser;
-
-  useEffect(() => {
-  if (!user) return;
-
-  const unsubscribe = onSnapshot(
-    collection(db, "users", user.uid, "shopping"),
-    (snapshot) => {
-      const lists: any[] = [];
-      snapshot.forEach((doc) => lists.push({ id: doc.id, ...doc.data() }));
-     setShoppingLists(lists);
-    }
-  );
-
-  return unsubscribe;
-}, []);
-
-
 
   const saveEvent = async () => {
     if (!eventTitle || !eventDate || !eventTime) {
@@ -132,7 +109,7 @@ const [shoppingItem, setShoppingItem] = useState("");
 };
 
 const saveTodo = async () => {
-    if (!todoTitle) {
+    if (!todoTitle || !todoPerson || !todoDate) {
       alert("Veuillez remplir tous les champs.");
       return;
     }
@@ -140,10 +117,14 @@ const saveTodo = async () => {
     try {
       await addDoc(collection(db, "todos"), {
         title: todoTitle,
+        person: todoPerson,
+        date: todoDate,
       });
 
       alert("Tâche sauvegardée !");
       setTodoTitle("");
+      setTodoPerson("");
+      setTodoDate("");
       setModalScreen(null);
       setMenuVisible(false);
 
@@ -153,39 +134,27 @@ const saveTodo = async () => {
   };
 
   const saveShopping = async () => {
-  if (!shoppingItem) {
-    alert("Veuillez remplir le champ produit.");
-    return;
-  }
-
-  try {
-    let listId = selectedListId;
-
-    if (!selectedListId) {
-      const newListRef = await addDoc(
-        collection(db, "users", user?.uid!, "shopping"),
-        { title: shoppingItem }
-      );
-      listId = newListRef.id;
+    if (!shoppingList || !shoppingItem) {
+      alert("Veuillez remplir tous les champs.");
+      return;
     }
 
-    await addDoc(
-      collection(db, "users", user?.uid!, "shopping", listId, "items"),
-      { name: shoppingItem, checked: false }
-    );
+    try {
+      await addDoc(collection(db, "shopping"), {
+        list: shoppingList,
+        item: shoppingItem,
+      });
 
-    alert("Produit ajouté !");
-    setShoppingItem("");
-    setSelectedListId("");
-    setNewListName("");
-    setModalScreen(null);
-    setMenuVisible(false);
-  } catch (err) {
-    alert("Impossible de sauvegarder.");
-  }
-};
+      alert("Liste de course sauvegardée !");
+      setShoppingList("");
+      setShoppingItem("");
+      setModalScreen(null);
+      setMenuVisible(false);
 
-
+    } catch (err) {
+      alert("Impossible de sauvegarder la liste de course.");
+    }
+  };
     switch (modalScreen) { 
 
     case "event":
@@ -196,6 +165,9 @@ const saveTodo = async () => {
           <TouchableOpacity onPress={goBack}>
               <Ionicons name="arrow-back-outline" size={26} color="#00d0ffff"/>
             </TouchableOpacity>
+            <TouchableOpacity onPress={closeModal}>
+              <Ionicons name="close-outline" size={26} color="red"/>
+            </TouchableOpacity>
             </View>
 
              <Text style={styles.modalTitle}>Nouvel Événement</Text>
@@ -204,7 +176,6 @@ const saveTodo = async () => {
           placeholder="Titre" 
           value={eventTitle} 
           onChangeText={setEventTitle} 
-      
           style={styles.inputWeb} />
           <input 
               type="date"
@@ -233,6 +204,9 @@ const saveTodo = async () => {
           <TouchableOpacity onPress={goBack}>
               <Ionicons name="arrow-back-outline" size={26} color="#00d0ffff"/>
             </TouchableOpacity>
+            <TouchableOpacity onPress={closeModal}>
+              <Ionicons name="close-outline" size={26} color="red"/>
+            </TouchableOpacity>
             </View>
 
           <Text style={styles.modalTitle}>Nouvelle Tâche</Text>
@@ -242,7 +216,17 @@ const saveTodo = async () => {
            value={todoTitle}
             onChangeText={setTodoTitle} 
             style={styles.inputWeb} />
-          
+          <TextInput 
+          placeholder="Responsable de la tâche" 
+          value={todoPerson} 
+          onChangeText={setTodoPerson} 
+          style={styles.inputWeb} />
+          <input 
+              type="date"
+              value={eventDate}
+              onChange={(e) => setEventDate(e.target.value)}  
+              style={styles.inputWeb}
+            />
 
             <TouchableOpacity style={styles.saveButton} onPress={saveTodo}>
             <Text style={styles.saveButtonText}>Sauvegarder</Text>
@@ -251,50 +235,38 @@ const saveTodo = async () => {
         </View>
       );
     case "shopping":
-  return (
-    <View style={styles.modalInnerContainer}>
-      <View style={styles.modalHeader}>
-        <TouchableOpacity onPress={goBack}>
-          <Ionicons name="arrow-back-outline" size={26} color="#00d0ff" />
-        </TouchableOpacity>
-      </View>
+      return (
+        <View style={styles.modalInnerContainer}>
 
-      <Text style={styles.modalTitle}>Nouvelle Liste de Course</Text>
+          <View style={styles.modalHeader}>
+          <TouchableOpacity onPress={goBack}>
+              <Ionicons name="arrow-back-outline" size={26} color="#00d0ffff"/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={closeModal}>
+              <Ionicons name="close-outline" size={26} color="red"/>
+            </TouchableOpacity>
+            </View>
 
-      <Text style={{ fontSize: 16, marginBottom: 5 }}>Choisir une liste existante</Text>
-      <Picker
-  selectedValue={selectedListId}
-  onValueChange={(val) => setSelectedListId(val)}
-  style={{ backgroundColor: "#f1f1f1", marginBottom: 10, borderRadius: 10 }}
->
-  <Picker.Item label="Créer une nouvelle liste" value="" />
-  {shoppingLists.map((list) => (
-    <Picker.Item key={list.id} label={list.title} value={list.id} />
-  ))}
-</Picker>
+          <Text style={styles.modalTitle}>Nouvelle Liste de Course</Text>
 
-      {!selectedListId && (
-        <TextInput
-          placeholder="Nom de la nouvelle liste"
-          value={newListName}
-          onChangeText={setNewListName}
-          style={styles.inputWeb}
-        />
-      )}
+          <TextInput 
+          placeholder="Nom de la liste" 
+          value={shoppingList} 
+          onChangeText={setShoppingList} 
+          style={styles.inputWeb} />
 
-      <TextInput
-        placeholder="Nom du produit"
-        value={shoppingItem}
-        onChangeText={setShoppingItem}
-        style={styles.inputWeb}
-      />
+          <TextInput
+           placeholder="Nom du produit" 
+           value={shoppingItem} 
+           onChangeText={setShoppingItem} 
+           style={styles.inputWeb} />
 
-      <TouchableOpacity style={styles.saveButton} onPress={saveShopping}>
-        <Text style={styles.saveButtonText}>Sauvegarder</Text>
-      </TouchableOpacity>
-    </View>
-  );
+           <TouchableOpacity style={styles.saveButton} onPress={saveShopping}>
+            <Text style={styles.saveButtonText}>Sauvegarder</Text>
+          </TouchableOpacity>
 
+        </View>
+      );
     default:
       return null;
   }
@@ -346,13 +318,6 @@ const saveTodo = async () => {
       <Modal visible={menuVisible} transparent animationType="slide">
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-            <TouchableOpacity
-        style={styles.closeModalButton}
-        onPress={() => setMenuVisible(false)}
-      >
-        <Ionicons name="close" size={15} color="black" />
-      </TouchableOpacity>
-
             {!modalScreen && (
         <View style={{ flexDirection: "row", justifyContent: "space-around", width: "100%", marginBottom: 20 }}>
           <TouchableOpacity style={styles.iconButton} onPress={() => setModalScreen("event")}>
@@ -421,8 +386,7 @@ const styles = StyleSheet.create({
 
   
   modalContent: {
-    width: "80%",
-    marginTop: 60,
+    width: "90%",
     backgroundColor: "#fff",
     borderRadius: 15,
     padding: 20,
@@ -524,13 +488,6 @@ saveButtonText: {
   color: "white",
   fontWeight: "bold",
   fontSize: 16,
-},
-closeModalButton: {
-  position: "absolute",
-  top: 10,
-  right: 10,
-  zIndex: 10,
-  padding: 5,
 },
 
 
--- components/AuthComponent.tsx
+++ components/AuthComponent.tsx
@@ -18,10 +18,25 @@ import {
 
   User
 } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { auth, db } from "../firebaseConfig";
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+const createUserIfNotExists = async (firebaseUser: User) => {
+  const ref = doc(db, "users", firebaseUser.uid);
+  const snap = await getDoc(ref);
+
+  if (!snap.exists()) {
+    await setDoc(ref, {
+      uid: firebaseUser.uid,
+      email: firebaseUser.email,
+      createdAt: Date.now(),
+    });
+    console.log("User Firestore créé");
+  } else {
+    console.log("User déjà existant Firestore");
+  }
+};
 
 export default function AuthComponent() {
 
@@ -108,64 +123,156 @@ WebBrowser.maybeCompleteAuthSession();
 
 
   const handleSignIn = async () => {
-  setErrorMessage("");
-  setEmailError(false);
-  setPasswordError(false);
 
-  let hasError = false;
-  if (!email) {
-    setEmailError(true);
-    hasError = true;
-  }
 
-  if (hasError) return;
+    setErrorMessage("");
 
-  setLoading(true);
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const uid = userCredential.user.uid;
 
-    await setDoc(doc(db, "users", uid), {
-      email: userCredential.user.email,
-      createdAt: new Date(),
-    }, { merge: true });
+    setEmailError(false);
 
-  } catch (error: any) {
-    if (error.code == 'auth/invalid-email') {
-      setErrorMessage("Email ou mot de passe incorrect");
-    } else {
-      setErrorMessage("Une erreur est survenue. Veuillez réessayer.");
+
+    setPasswordError(false);
+
+
+
+
+
+    let hasError = false;
+
+
+    if (!email) {
+
+
+      setEmailError(true);
+
+
+      hasError = true;
+
+
     }
-  } finally {
-    setLoading(false);
-  }
-};
+
+
+    
+
+
+    if (hasError) return;
+
+
+
+
+
+
+
+
+    setLoading(true);
+
+    try {
+
+      await signInWithEmailAndPassword(auth, email, password);
+      const currentUser = auth.currentUser;
+if (currentUser) {
+  await createUserIfNotExists(currentUser);
+}
+    } catch (error: any) {
+
+
+      if (
+
+
+        error.code == 'auth/invalid-email'
+
+
+      ) {
+
+
+        setErrorMessage("Email ou mot de passe incorrect");
+
+
+      } else {
+
+
+        setErrorMessage("Une erreur est survenue. Veuillez réessayer.");
+
+
+      } 
+
+
+    } finally {
+
+
+      setLoading(false);
+
+    }
+
+  };
+
 
   useEffect(() => {
-  if (response?.type === 'success') {
-    const { authentication } = response;
-    const idToken = authentication?.idToken;
 
-    if (idToken) {
-      const credential = GoogleAuthProvider.credential(idToken);
-      (async () => {
 
-        const result = await signInWithCredential(auth, credential);
-        const user = result.user; 
-        const uid = user.uid;
+    if (response?.type === 'success') {
 
-        await setDoc(doc(db, "users", uid), {
-          email: user.email,
-          createdAt: new Date(),
-        }, { merge: true });
 
-        console.log("Connexion Google réussie");
-      })();
+      const { authentication } = response;
+
+
+      const idToken = authentication?.idToken;
+
+
+      if (idToken) {
+
+
+        const credential = GoogleAuthProvider.credential(idToken);
+
+
+        (async () => {
+
+
+          try {
+
+
+            await signInWithCredential(auth, credential);
+            if (auth.currentUser) {
+          await createUserIfNotExists(auth.currentUser);
+}
+
+            // router.push('/drawer/Acceuil');
+
+
+            console.log("Connexion réussie");
+
+
+          } catch (err: any) {
+
+
+            console.warn('Firebase signInWithCredential error', err);
+ 
+
+            Alert.alert('Erreur', err.message || String(err));
+
+
+          }
+
+
+        })();
+
+
+      }
+
+
     }
-  } else if (response?.type === 'error') {
-    Alert.alert('Erreur', 'Échec de la connexion Google. Veuillez réessayer.');
-  }
-}, [response]);
+
+
+    else if (response?.type === 'error') {
+
+
+      Alert.alert('Erreur', 'Échec de la connexion Google. Veuillez réessayer.');
+
+
+    }
+
+
+  }, [response]);
 
 
 
