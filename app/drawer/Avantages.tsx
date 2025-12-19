849b318755381dd573d1d07f7e6cf9f4ab09eac4
amaurystra123-jpg <amaurystra123@gmail.com>
2025-12-19 13:21:12 +0100

    maj 5 19/12/2025

--- app/drawer/Avantages.tsx
+++ app/drawer/Avantages.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { DrawerActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Avantages() {
+import { TouchableOpacity, } from "react-native";
+export function Avantages() {
   return (
-    <View>
-      <Text>Avantages</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}> Avantages</Text>
     </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+export default function () {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="ProfilMain"
+        component={Avantages}
+        options={({ navigation }) => ({
+          headerTitle: "Mes avantages",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
+              <Ionicons name="menu" size={26} style={{ marginLeft: 15 }} />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 20, fontWeight: "bold", marginBottom: 10 },
+});
--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -10,19 +10,11 @@ export default function DrawerLayout() {
 
 
     <Drawer screenOptions={{ headerShown: false }}>
-
       <Drawer.Screen name="Acceuil" options={{ title: "Accueil" }} />
-
       <Drawer.Screen name="Budget" options={{ title: "Budget" }} />
-
       <Drawer.Screen name="Avantages" options={{ title: "Avantages" }} />
-
       <Drawer.Screen name="profil" options={{ title: "Profil" }} />
-      
       <Drawer.Screen name= "Famille"  options = {{title: "Famille"}} />
-
       <Drawer.Screen name="Parametres" options={{ title: "Paramètres" }} />
-
       <Drawer.Screen name="Déconnexion" options={{ title: "Déconnexion" }} />
-
     </Drawer>)}
