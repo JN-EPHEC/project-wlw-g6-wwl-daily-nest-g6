aa72ab16b626b0d1157407bde4e31d78a8ba3276
FZ-R <framhani@gmail.com>
2026-01-01 19:10:27 +0100

    import/application des polices

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,24 @@
-import "../global.css";
 import { Slot } from "expo-router";
+import "react-native-reanimated";
+import "../global.css";
+
+import {
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+} from "@expo-google-fonts/montserrat";
+import { Shrikhand_400Regular } from "@expo-google-fonts/shrikhand";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
-  return (
-    <Slot />
-  );
+  const [fontsLoaded] = useFonts({
+    Shrikhand_400Regular,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+  });
+
+  if (!fontsLoaded) return null;
+
+  return <Slot />;
 }
