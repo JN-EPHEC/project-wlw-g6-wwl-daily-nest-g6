4e88fa53922c28192fe7c374c7fc38a4190124c0
Amine Aydogan <a.aydogan@students.ephec.be>
2026-01-05 16:20:55 +0100

    benefices

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ import {
 } from "@expo-google-fonts/montserrat";
 import { Shrikhand_400Regular } from "@expo-google-fonts/shrikhand";
 import { useFonts } from "expo-font";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -18,7 +19,26 @@ export default function RootLayout() {
     Montserrat_600SemiBold,
   });
 
-  if (!fontsLoaded) return null;
+  return (
+    <View style={{ flex: 1 }}>
+      {/* ✅ Slot TOUJOURS monté */}
+      <Slot />
 
-  return <Slot />;
+      {/* Petit overlay pendant que les fonts chargent */}
+      {!fontsLoaded && (
+        <View style={styles.loadingOverlay}>
+          <ActivityIndicator size="large" />
+        </View>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  loadingOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+});
