d22819736274db29fa7066175dabe92ce577b317
lux2as <lucas13gerard@gmail.com>
2026-01-11 14:27:04 +0100

    Nouveau Drawer

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -37,10 +37,11 @@ function DrawerRow({
       className="flex-row items-center px-4 py-3 rounded-2xl mb-2 bg-[#F8F9FA]"
     >
       <View className="w-9 h-9 rounded-xl items-center justify-center mr-3" style={{ backgroundColor: `${color}22` }}>
-        <Ionicons name={icon} size={18} color={color} />
+        <Ionicons name={icon} size={20} color={color} />
       </View>
 
-      <Text className="flex-1 text-[15px] font-semibold text-[#111827]">{label}</Text>
+      <Text className="flex-1 text-[15px] font-semibold text-[#111827]"
+      style={{ fontFamily: "Montserrat_400Regular", fontSize: 14}}>{label}</Text>
 
       {badge ? (
         <View className="px-2 py-0.5 rounded-full" style={{ backgroundColor: badgeColor }}>
@@ -99,7 +100,7 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
       {/* Header */}
       <View
         className="pt-16 pb-6 px-6"
-        style={{ backgroundColor: "#FF914D", borderBottomRightRadius: 32 }}
+        style={{ backgroundColor: "#6DDB31", borderBottomRightRadius: 0 }}
       >
         <View className="flex-row items-center mb-4">
           <View
@@ -112,10 +113,12 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
           </View>
 
           <View className="flex-1">
-            <Text className="text-white text-[20px] font-bold mb-1">
+            <Text className="text-white text-[20px] font-bold mb-1"
+            style={{ fontFamily: "Shrikhand_400Regular", fontWeight: "300" }}>
               Bonjour {firstName ? `${firstName}!` : "!"}
             </Text>
-            <Text className="text-white/90 text-[12px] whitespace-pre mt-2">
+            <Text className="text-white/90 text-[12px] whitespace-pre mt-2"
+            style={{ fontFamily: "Montserrat_400Regular", fontWeight: "500" }}>
               {userEmail ?? "Utilisateur connecté"}
             </Text>
           </View>
@@ -139,13 +142,14 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
 
       <DrawerContentScrollView
         {...props}
-        contentContainerStyle={{ paddingTop: 16, paddingBottom: 16 }}
+        contentContainerStyle={{ paddingTop: 16, paddingBottom: 20 }}
         showsVerticalScrollIndicator={false}
       >
         {/* liste sep (peut mettre des notifs mais azy)
             */}
         <View className="px-4 mb-4">
-          <Text className="text-[11px] font-bold text-[#9CA3AF] uppercase mb-3 px-2">
+          <Text className="text-[11px] font-bold text-[#9CA3AF] uppercase mb-3 px-2"
+          style={{ fontFamily: "Montserrat_400Regular" }}>
             Principal
           </Text>
 
@@ -165,20 +169,21 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
           <DrawerRow
             icon="gift-outline"
             label="Avantages"
-            color="#ABF085"
+            color="#6DDB31"
             onPress={() => props.navigation.navigate("Avantages")}
           />
         </View>
 
         <View className="px-4 mb-4">
-          <Text className="text-[11px] font-bold text-[#9CA3AF] uppercase mb-3 px-2">
+          <Text className="text-[11px] font-bold text-[#9CA3AF] uppercase mb-3 px-2"
+          style={{ fontFamily: "Montserrat_400Regular" }}>
             Famille
           </Text>
 
           <DrawerRow
             icon="people-outline"
             label="Famille"
-            color="#7CB368"
+            color="#6DDB31"
             onPress={() => props.navigation.navigate("Famille")}
           />
           <DrawerRow
@@ -192,7 +197,8 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
         </View>
 
         <View className="px-4 mb-4">
-          <Text className="text-[11px] font-bold text-[#9CA3AF] uppercase mb-3 px-2">
+          <Text className="text-[11px] font-bold text-[#9CA3AF] uppercase mb-3 px-2"
+          style={{ fontFamily: "Montserrat_400Regular" }}>
             Compte
           </Text>
 
@@ -231,8 +237,10 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
       <View className="px-6 py-4 border-t border-[#F1F3F5]">
         <View className="flex-row items-center justify-between">
           <View>
-            <Text className="text-[12px] font-semibold text-[#111827]">Daily Nest</Text>
-            <Text className="text-[11px] text-[#9CA3AF]">Version 1.0.0</Text>
+            <Text className="text-[12px] font-semibold text-[#111827]"
+            style={{ fontFamily: "Montserrat_400Regular" }}>Daily Nest</Text>
+            <Text className="text-[11px] text-[#9CA3AF]"
+            style={{ fontFamily: "Montserrat_400Regular" }}>Version 1.0.0</Text>
           </View>
 
         </View>
@@ -248,7 +256,7 @@ export default function DrawerLayout() {
         drawerContent={(props) => <CustomDrawerContent {...props} />}
         screenOptions={{
           headerShown: false,
-          drawerStyle: { backgroundColor: "white", width: 300 },
+          drawerStyle: { backgroundColor: "#fff", width: 300 },
         }}
       >
         <Drawer.Screen name="Accueil" options={{ title: "Accueil" }} />
